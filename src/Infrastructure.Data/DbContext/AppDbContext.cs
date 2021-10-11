c117990dfd385d84d6bf6262d3aefa527cc32057
Richard Chukwuma <richardchukwuma99@gmail.com>
2021-10-11 13:16:37 +0100

    comment on appdbcontext

--- src/Infrastructure.Data/DbContext/AppDbContext.cs
+++ src/Infrastructure.Data/DbContext/AppDbContext.cs
@@ -4,6 +4,10 @@ namespace Infrastucture.Data.DbContext
 {
     public class AppDbContext: Microsoft.EntityFrameworkCore.DbContext
     {
+        /// <summary>
+        /// Dbcontext comment
+        /// </summary>
+        /// <param name="options"></param>
         public AppDbContext(DbContextOptions options) : base(options)
         {
 
